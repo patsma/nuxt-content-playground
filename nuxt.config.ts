bb660405b5de9442afb426e4b88a296715b8873a
Patryk Smakosz <patryksmakosz1@gmail.com>
2025-02-21 20:45:40 +0100

    chore: Add Netlify CLI and configure Nuxt Content

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,4 +8,13 @@ export default defineNuxtConfig({
     compatibilityVersion: 4,
   },
   compatibilityDate: '2024-04-03',
+  content: {
+    preview: {
+      api: 'https://api.nuxt.studio',
+    },
+    documentDriven: true,
+    renderer: {
+      anchorLinks: false,
+    },
+  },
 })

f6a92073fb553acaa2dc7ac24b20d2679d583c75
Patryk Smakosz <patryksmakosz1@gmail.com>
2025-04-24 06:01:04 +0200

    test again

--- content.config.ts
+++ content.config.ts
@@ -8,7 +8,7 @@ export default defineContentConfig({
     }),
     navigation: defineCollection({
       type: 'data',
-      source: 'content/navigation1.json',
+      source: 'content/navigation.json',
       schema: z.object({
         items: z.array(
           z.object({
